d64bcdc96aece20b5a41491219695655ac433023
hirejohnloveland <hirejohnloveland@gmail.com>
2021-08-10 16:13:01 -0500

    bypass secret key for student deploy

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config():
     FLASK_APP = os.environ.get('FLASK_APP')
     FLASK_ENV = os.environ.get('FLASK_ENV')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'testkey'
     if not SECRET_KEY:
         raise ValueError("Secret Key Not Provided, Please add to .env file")
 
