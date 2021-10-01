dbea9b1f369be4231db119287ee24d3850314048
hirejohnloveland <hirejohnloveland@gmail.com>
2021-10-01 13:43:51 -0500

    migration to Postrgresql

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ app = create_app()
 
 
 
-# The following code allows you to start the Flask shell with some items already imported and ready to run.
-# It's primary purpose is to allow me to build the SQLite database during initial set up and testing. 
+# The following code allows you to start the Flask Postgresql database during initial set up and testing. 
 from app.blueprints.users.models import User
 from app.blueprints.products.models import Product
 from app.blueprints.products.db_init import populate_products
--- config.py
+++ config.py
@@ -5,12 +5,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config():
     FLASK_APP = os.environ.get('FLASK_APP')
     FLASK_ENV = os.environ.get('FLASK_ENV')
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'testkey'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     if not SECRET_KEY:
         raise ValueError("Secret Key Not Provided, Please add to .env file")
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get(
         'SQLALCHEMY_TRACK_MODIFICATIONS')
     MAIL_SERVER = 'smtp.sendgrid.net'
